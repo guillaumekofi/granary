79daae155a866ec415ca6fae1c60123753ec741c
guillaumekofi <guillaume.kofi@gmail.com>
2022-08-10 01:09:58 -0500

    components: updated routes in App

--- granary-app/src/App.js
+++ granary-app/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NewsFeed from "./pages/newsFeed/NewsFeed";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className="container">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<NewsFeed />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
