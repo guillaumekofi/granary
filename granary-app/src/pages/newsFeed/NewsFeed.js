242f184d5f9291f4c32e07c3d7066bee4fcfa351
guillaumekofi <guillaume.kofi@gmail.com>
2022-08-10 01:14:06 -0500

    components: added title

---
+++ granary-app/src/pages/newsFeed/NewsFeed.js
@@ -0,0 +1,15 @@
+// import styles
+import './NewFeed.css'
+
+// import {useState} from "react";
+
+const NewsFeed = () => {
+
+  return (
+    <div>
+      <h2 className="page-title">My NewsFeed</h2>
+    </div>
+  );
+};
+
+export default NewsFeed
