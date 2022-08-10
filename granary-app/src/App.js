7baaabd208d35888c8c7b38d46353572ca649a7d
guillaumekofi <guillaume.kofi@gmail.com>
2022-08-10 01:42:21 -0500

    components: added Sidebar to App

--- granary-app/src/App.js
+++ granary-app/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NewsFeed from "./pages/newsFeed/NewsFeed";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Sidebar />
         <div className="container">
           <Navbar />
           <Routes>
