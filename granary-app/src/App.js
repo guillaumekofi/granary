758787574e4ae1a4a540774c9c5701f4f195edf4
guillaumekofi <guillaume.kofi@gmail.com>
2022-08-13 02:34:23 -0500

    components: added Login to App

--- granary-app/src/App.js
+++ granary-app/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NewsFeed from "./pages/newsFeed/NewsFeed";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Signup from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<NewsFeed />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ granary-app/src/pages/login/Login.js
@@ -0,0 +1,53 @@
+// app components import
+import Alert from "../../components/Alert";
+
+// react component import
+import {useState} from "react";
+import useLogin from "../../hooks/useLogin";
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  //get useLogin hook returns
+  const {error, isPending, login} = useLogin();
+
+  // execute login function call
+  // and log user in if everything is clear
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevent browser default behavior
+    // call login function
+    login(email, password);
+  }
+
+  return (
+    // login form
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      {/*display error*/}
+      {error && <Alert type="error" message={error}/>}
+
+      {/* show loading if isPending or show signup button*/}
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && <p className="text-info">Please wait, We are logging you in ...</p>}
+    </form>
+  );
+};
+
+export default Login;
