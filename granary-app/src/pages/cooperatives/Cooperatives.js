89d643e25c66e5654e2c0f7b2bdba3b2c43a660f
guillaumekofi <guillaume.kofi@gmail.com>
2022-08-16 18:16:23 -0500

    pages: added Cooperatives page

---
+++ granary-app/src/pages/cooperatives/Cooperatives.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cooperatives = () => {
+  return (
+    <div>
+      <h2 className="page-title">My Cooperatives</h2>
+    </div>
+  );
+};
+
+export default Cooperatives;
