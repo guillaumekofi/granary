c656c21b57d35ac7afb4136e77e0fa5736ba15c7
guillaumekofi <guillaume.kofi@gmail.com>
2022-08-16 18:12:23 -0500

    components: added authContext and safeguarded routes in app

--- granary-app/src/App.js
+++ granary-app/src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import NewsFeed from "./pages/newsFeed/NewsFeed";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
+import {useAuthContext} from "./hooks/useAuthContext";
+import Cooperatives from "./pages/cooperatives/Cooperatives";
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Sidebar />
-        <div className="container">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<NewsFeed />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+
+      {authIsReady && (
+        <BrowserRouter>
+          {user && <Sidebar />}
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={user ? <NewsFeed /> : <Navigate to="/login" />} />
+              <Route path="/cooperatives" element={user ? <Cooperatives /> : <Navigate to="/login" />} />
+
+              {/* auth routes */}
+              <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
+              <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
