6cc0fd6802d9babd30603744139c0031bdb3deed
guillaumekofi <guillaume.kofi@gmail.com>
2022-08-16 18:19:23 -0500

    components: updated Navbar with safeguarded links

--- granary-app/src/components/Navbar.js
+++ granary-app/src/components/Navbar.js
@@ -4,8 +4,13 @@ import Logo from '../assets/logos/agriculture.svg'
 
 // import react components
 import {Link} from "react-router-dom";
+import useLogout from "../hooks/useLogout";
+import {useAuthContext} from "../hooks/useAuthContext";
 
 const Navbar = () => {
+  const {error, isPending, logout} = useLogout();
+  const { user } = useAuthContext();  // get user to handle navbar menu
+
   return (
     <div className="navbar">
       <ul>
@@ -18,12 +23,20 @@ const Navbar = () => {
         </li>
 
         {/*login, signup and logout button*/}
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
-        <button className="btn">Logout</button>
-
+        {!user &&
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>
+        }
+        {user &&
+          <>
+            {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+            {isPending && <p className="text-info">Logging you out...</p>}
+            {error && <p>{error}</p>}
+          </>
+        }
       </ul>
-
     </div>
   );
 };
