68fbe93d19c545d92dbc2c7cd1590f5b50da76ea
guillaumekofi <guillaume.kofi@gmail.com>
2022-08-23 01:34:39 -0500

    hooks: changed the useUsername to useUserData hook

---
+++ granary-app/src/hooks/useUserData.js
@@ -0,0 +1,39 @@
+import {useEffect, useState} from "react";
+
+import {doc, onSnapshot} from 'firebase/firestore';
+import {db} from "../firebase/config";
+
+// get the user info from the users collection
+// for display in sidebar and other pages/components
+export const useUserData = (collection, userId) => {
+  const [error, setError] = useState(null);  // catch errors
+  const [username, setUsername] = useState('')  // username initial state
+  const [dateJoined, setDateJoined] = useState('')  // date joined initial state
+
+  // realtime data for user document by current user uid
+  useEffect(() => {
+    // get doc reference
+    const docRef = doc(db, collection, userId);
+
+    // snapshot the docRef to get the user doc and username
+    const unsubscribe = onSnapshot(docRef, (snapshot) => {
+      if (snapshot.data()) {
+        // set username if data exists
+        setUsername(snapshot.data().username);
+        // set date joined
+        setDateJoined(snapshot.data().dateJoined.toDate().toDateString());
+        setError(null); // no error
+      } else {
+        setError("Something went wrong!");
+      }
+    }, (err) => {
+      setError('Failed to get document');
+    })
+
+    return () => unsubscribe()
+
+  }, [collection, userId])
+
+  // return user data
+  return { dateJoined, username, error }
+};
