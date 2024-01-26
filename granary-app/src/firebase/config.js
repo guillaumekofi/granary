87cbc36988f8b25c174adbce9ea1099c9614a33b
Kofi <guillaume.kofi@gmail.com>
2024-01-26 06:17:12 -0600

    Update config.js

--- granary-app/src/firebase/config.js
+++ granary-app/src/firebase/config.js
@@ -5,13 +5,13 @@ import {getStorage} from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBVCZah_mmV1FzmGMywYl2dzzqkUw6JytQ",
-  authDomain: "granary-19b23.firebaseapp.com",
-  databaseURL: "https://granary-19b23-default-rtdb.firebaseio.com",
-  projectId: "granary-19b23",
-  storageBucket: "granary-19b23.appspot.com",
-  messagingSenderId: "1067468673886",
-  appId: "1:1067468673886:web:3817aca004bcd69b4375cf"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
